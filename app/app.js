bc5660433656292c7197d3c13ffd680dd2e9878d
nlread <nedlyfordread@gmail.com>
2017-04-18 19:53:23 -0500

    Server static files

--- app/app.js
+++ app/app.js
@@ -1,9 +1,12 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/static/index.html');
 });
+
+app.use(express.static(__dirname + '/static'));
 
 http.listen(3000, function() {
    console.log('listening on *:3000'); 
