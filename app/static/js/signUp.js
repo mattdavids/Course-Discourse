15f325859c22f4c13d55280c19b7179248bb8bec
mattdavids <mdavids@macalester.edu>
2017-04-18 23:08:47 -0500

    Added angular for email and major signup

--- app/static/js/signUp.js
+++ app/static/js/signUp.js
@@ -2,7 +2,7 @@ myApp = angular.module('signUp', ['ngRoute']);
 
 myApp.config(function($routeProvider) {
     $routeProvider.when('/', {
-        template: '<email-password></email-password>'
+        template: '<name-major></name-major>'
     }).otherwise('/');
 });
 
@@ -15,15 +15,68 @@ myApp.component('headerTop', {
 
 myApp.component('emailPassword', {
     templateUrl: 'emailPassword.template.html',
-    controller: function($scope, whichPage) {
+    controller: function($scope, $http, whichPage) {
         whichPage.set('Email and Password');
+        
+        $scope.user = {};
+        
+        $scope.submitForm = function() {
+            $http({
+                method: 'POST', 
+                url: '/',
+                data: $scope.user,
+                headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+            });
+        }
     }
 });
 
 myApp.component('nameMajor', {
     templateUrl: 'nameMajor.template.html',
-    controller: function($scope, whichPage) {
+    controller: function($scope, $http, whichPage) {
+        
         whichPage.set('Name and Major');
+        
+        $scope.majors = ['CompSci', 'Anthropology', 'Linguistics'];
+        $scope.minors = ['CompSci', 'Math', 'Data Science'];
+        $scope.user = {};
+        
+        let numMajors = 1;
+        let numMinors = 0;
+        
+        $scope.majorItems = [{
+            majorNum: 1,
+            model: 'user.major1'
+        }];
+        
+        $scope.minorItems = [];
+        
+        $scope.addNewMajor = function() {
+            numMajors += 1;
+            $scope.majorItems.push({
+                majorNum: numMajors,
+                model: 'user.major' + numMajors,
+            });
+        }
+        
+        $scope.addNewMinor = function() {
+            numMinors += 1;
+            $scope.minorItems.push({
+                minorNum: numMinors,
+                model: 'user.minor' + numMinors,
+            });
+        }
+        
+        $scope.submitForm = function() {
+            $http({
+                method: 'POST', 
+                url: '/',
+                data: $scope.user,
+                headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+            });
+        }
+        
+
     }
 });
 
