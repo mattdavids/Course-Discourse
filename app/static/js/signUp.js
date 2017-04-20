6b62d7bfd7f4b08328ef0374364b6faefb41da91
mattdavids <mdavids@macalester.edu>
2017-04-19 21:50:54 -0500

    Added pages for inputting name, interests, and majors

--- app/static/js/signUp.js
+++ app/static/js/signUp.js
@@ -2,7 +2,13 @@ myApp = angular.module('signUp', ['ngRoute']);
 
 myApp.config(function($routeProvider) {
     $routeProvider.when('/', {
+        template: '<email-password></email-password>'
+    }).when('/major', {
         template: '<name-major></name-major>'
+    }).when('/interests', {
+        template: '<interest-select></interest-select>'
+    }).when('/classes', {
+        template: '<class-select></class-select>'
     }).otherwise('/');
 });
 
@@ -15,9 +21,8 @@ myApp.component('headerTop', {
 
 myApp.component('emailPassword', {
     templateUrl: 'emailPassword.template.html',
-    controller: function($scope, $http, whichPage) {
-        whichPage.set('Email and Password');
-        
+    controller: function($scope, $http, $location, whichPage) {
+        whichPage.set('Name, Email, and Password');
         $scope.user = {};
         
         $scope.submitForm = function() {
@@ -27,45 +32,89 @@ myApp.component('emailPassword', {
                 data: $scope.user,
                 headers: {'Content-Type': 'application/x-www-form-urlencoded'}
             });
+            $location.path('/major');
         }
     }
 });
 
 myApp.component('nameMajor', {
     templateUrl: 'nameMajor.template.html',
-    controller: function($scope, $http, whichPage) {
+    controller: function($scope, $http, $location, whichPage) {
         
-        whichPage.set('Name and Major');
+        whichPage.set('Majors and Minors');
         
-        $scope.majors = ['CompSci', 'Anthropology', 'Linguistics'];
-        $scope.minors = ['CompSci', 'Math', 'Data Science'];
         $scope.user = {};
         
-        let numMajors = 1;
-        let numMinors = 0;
+        $scope.currentMajors = [];
+        $scope.currentMinors = [];
         
-        $scope.majorItems = [{
-            majorNum: 1,
-            model: 'user.major1'
-        }];
+        $scope.majors = ['Academics', 'Reading', 'Hearthstone', 'Dogs', 'Cats', 'Table Tennis', 'Miniature Sports', 'Model Trains', 'War Gaming', 'Gaming', 'Origami', 'Competative Battleship', 'Jogging', 'Competative Eating', 'Computers', 'Street Fighting', 'Street Fighter', 'Back Alley Brawls'];       
+        $scope.minors = ['CompSci', 'Math', 'Data Science'];
         
-        $scope.minorItems = [];
+        $scope.search = {
+            minorVal: '',
+            majorVal: ''
+        };
         
-        $scope.addNewMajor = function() {
-            numMajors += 1;
-            $scope.majorItems.push({
-                majorNum: numMajors,
-                model: 'user.major' + numMajors,
+        $scope.addMajor = function(major) {
+            $scope.currentMajors.push(major);
+            $scope.majors.splice($scope.majors.indexOf(major), 1);
+            $scope.updateMajorSearch();
+            $scope.currentMajors.sort(function(a, b) {
+                return a.toLowerCase().localeCompare(b.toLowerCase());
             });
         }
         
-        $scope.addNewMinor = function() {
-            numMinors += 1;
-            $scope.minorItems.push({
-                minorNum: numMinors,
-                model: 'user.minor' + numMinors,
+        $scope.deleteMajor = function(major) {
+            $scope.currentMajors.splice($scope.currentMajors.indexOf(major), 1);
+            $scope.majors.push(major);
+            $scope.updateMajorSearch();
+        }
+        
+        $scope.updateMajorSearch = function() {
+            $scope.majorsResult = $scope.majors.filter(function(term) {
+                return term.toLowerCase().includes($scope.search.majorVal.toLowerCase());
+            }).sort(function(a, b) {
+                return a.toLowerCase().localeCompare(b.toLowerCase());
+            }).slice(0, 5);  
+        }
+        
+        $scope.addMinor = function(minor) {
+            $scope.currentMinors.push(minor);
+            $scope.minors.splice($scope.minors.indexOf(minor), 1);
+            $scope.updateMinorSearch();
+            $scope.currentMinors.sort(function(a, b) {
+                return a.toLowerCase().localeCompare(b.toLowerCase());
             });
         }
+        
+        $scope.deleteMinor = function(minor) {
+            $scope.currentMinors.splice($scope.currentMinors.indexOf(minor), 1);
+            $scope.minors.push(minor);
+            $scope.updateMinorSearch();
+        }
+        
+        $scope.updateMinorSearch = function() {
+            $scope.minorsResult = $scope.minors.filter(function(term) {
+                return term.toLowerCase().includes($scope.search.minorVal.toLowerCase());
+            }).sort(function(a, b) {
+                return a.toLowerCase().localeCompare(b.toLowerCase());
+            }).slice(0, 5);  
+        }
+        
+        $scope.submitForm = function() {
+            $scope.user.currentMajors = $scope.currentMajors;
+            $scope.user.currenMinors = $scope.currentMinors;
+            $http({
+                method: 'POST', 
+                url: '/',
+                data: $scope.user,
+                headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+            });
+            $location.path('/clubs');
+        }
+        $scope.updateMajorSearch();
+        $scope.updateMinorSearch();
         
         $scope.submitForm = function() {
             $http({
@@ -74,6 +123,7 @@ myApp.component('nameMajor', {
                 data: $scope.user,
                 headers: {'Content-Type': 'application/x-www-form-urlencoded'}
             });
+            $location.path('/interests');
         }
         
 
@@ -82,8 +132,82 @@ myApp.component('nameMajor', {
 
 myApp.component('interestSelect', {
     templateUrl: 'interestSelect.template.html',
-    controller: function($scope, whichPage) {
-        whichPage.set('Clubs and Interests');
+    controller: function($scope, $http, $location, whichPage) {
+        whichPage.set('Select Interests and Clubs');
+        
+        $scope.currentInterests = [];
+        
+        $scope.interests = ['Academics', 'Reading', 'Hearthstone', 'Dogs', 'Cats', 'Table Tennis', 'Miniature Sports', 'Model Trains', 'War Gaming', 'Gaming', 'Origami', 'Competative Battleship', 'Jogging', 'Competative Eating', 'Computers', 'Street Fighting', 'Street Fighter', 'Back Alley Brawls'];       
+        
+        $scope.currentClubs = [];
+        
+        $scope.clubs = ['Bridge', 'Mac Weekly', 'Anime', 'Yarn', 'Fossil Free', 'Minnesota Public Interest Group']; 
+        
+        $scope.search = {
+            interestsVal: '',
+            clubVal: ''
+        };
+        
+        $scope.addInterest = function(interest) {
+            $scope.currentInterests.push(interest);
+            $scope.interests.splice($scope.interests.indexOf(interest), 1);
+            $scope.updateInterestSearch();
+            $scope.currentInterests.sort(function(a, b) {
+                return a.toLowerCase().localeCompare(b.toLowerCase());
+            });
+        }
+        
+        $scope.deleteInterest = function(interest) {
+            $scope.currentInterests.splice($scope.currentInterests.indexOf(interest), 1);
+            $scope.interests.push(interest);
+            $scope.updateInterestSearch();
+        }
+        
+        $scope.updateInterestSearch = function() {
+            $scope.interestsResult = $scope.interests.filter(function(term) {
+                return term.toLowerCase().includes($scope.search.interestsVal.toLowerCase());
+            }).sort(function(a, b) {
+                return a.toLowerCase().localeCompare(b.toLowerCase());
+            }).slice(0, 5);  
+        }
+        
+        $scope.addClub = function(club) {
+            $scope.currentClubs.push(club);
+            $scope.clubs.splice($scope.clubs.indexOf(club), 1);
+            $scope.updateClubSearch();
+            $scope.currentClubs.sort(function(a, b) {
+                return a.toLowerCase().localeCompare(b.toLowerCase());
+            });
+        };
+        
+        $scope.deleteClub = function(club) {
+            $scope.currentClubs.splice($scope.currentClubs.indexOf(club), 1);
+            $scope.clubs.push(club);
+            $scope.updateClubSearch();
+        }
+        
+        $scope.updateClubSearch = function() {
+            $scope.clubsResult = $scope.clubs.filter(function(term) {
+                return term.toLowerCase().includes($scope.search.clubVal);
+            }).sort(function(a, b) {
+                return a.toLowerCase().localeCompare(b.toLowerCase());
+            }).slice(0, 5);
+        }
+        
+        $scope.submitForm = function() {
+            $http({
+                method: 'POST', 
+                url: '/',
+                data: {
+                    Interests: $scope.currentInterests,
+                    Clubs: $scope.currentClubs
+                },
+                headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+            });
+            $location.path('/classes');
+        }
+        $scope.updateInterestSearch();
+        $scope.updateClubSearch();
     }
 });
 
