ca1a75b9266744cfc222dfb91f42a45c6d4ad514
mattdavids <mdavids@macalester.edu>
2017-04-19 21:51:12 -0500

    Made the app able to serve login and signup from the index

--- app/app.js
+++ app/app.js
@@ -6,6 +6,14 @@ app.get('/', function(req, res) {
     res.sendFile(__dirname + '/static/index.html');
 });
 
+app.get('/signup', function(req, res) {
+    res.sendFile(__dirname + '/static/signUp.html');
+});
+
+app.get('/login', function(req, res) {
+    res.sendFile(__dirname + '/static/login.html');
+});
+
 app.use(express.static(__dirname + '/static'));
 
 http.listen(3000, function() {
